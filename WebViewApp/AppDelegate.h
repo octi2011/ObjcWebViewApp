4b733c725b40536171e54f0a7862832b65fd6a0c
Duminica Octavian <duminicaoctavian@192-168-0-101.rdsnet.ro>
2018-03-23 13:14:28 +0200

    Initial Commit

---
+++ WebViewApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebViewApp
+//
+//  Created by Duminica Octavian on 23/03/2018.
+//  Copyright © 2018 Duminica Octavian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebViewApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebViewApp
+//
+//  Created by Duminica Octavian on 23/03/2018.
+//  Copyright © 2018 Duminica Octavian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
